fe3087cee0cea4fbd7eeabec29c85999d322e481
Christian <christian.jena@gmail.com>
2021-04-22 15:52:30 +0200

    working prototype

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# seqrs
-sequence repair
+# SEQuence Repair via rUst
+* if you are doing nanopore amplicon sequencing vor SARS-CoV-2 read on, else leave this git :)
+* This small parser/tool aims to tell you which minimum set of primer would be need to repair a genome
+* the main goal was to learn rust a bit so this git is super basic
 
 ## Installation
+* when iam finished it will be a prober executable currently it can be run via rusts cargo
 * install rust - done
 
 ```bash
@@ -11,4 +14,40 @@ sudo apt install build-essential
 curl https://sh.rustup.rs -sSf | sh
 # refresh $PATH or restart terminal
 . ~/.profile
+# clone the git
+git clone https://github.com/replikation/seqrs.git
 ```     
+
+## run
+
+````bash
+# navigate into the git
+cd seqrs/
+# execute via cargo
+cargo run -- --genomes data/multifasta_v1200.fasta --primerbed data/Primerfiles/V1200/nCoV-2019.bed --results results.txt
+````
+
+## results
+you get a tsv file with `fastaheader primername`
+
+## help
+
+`cargo run -- --help`
+
+````bash
+seqrs - sequence repair in rust 0.2.0
+Quickly extract primerpairs to amplify missing/masked regions of genomes.
+
+USAGE:
+    seqrs [OPTIONS] --genomes <genomes> --primerbed <primerbed>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -a, --ampliconsize <ampliconsize>    amplicon size [default: 1200]
+    -g, --genomes <genomes>              Fasta file input
+    -p, --primerbed <primerbed>          bed file containing the primer infos
+    -r, --results <results>              tab separated output [default: results.tsv]
+````
--- src/main.rs
+++ src/main.rs
@@ -8,44 +8,41 @@ they are autodownloaded when compiling
 extern crate bio;
 
 /// libraries
-use structopt::StructOpt; // easy to use args via the #[derive]
+use structopt::StructOpt; // https://docs.rs/structopt/0.3.21/structopt/
 use anyhow::{Context, Result}; // for clean as fuck error reports
-//use std::path::PathBuf;
-//use std::io::{BufReader};
+
+// std stuff
 use std::fs::File;
-use std::fs;
+//use std::fs;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 /// rust-bio
 use bio::io::fasta;
 use bio::io::bed;
-// use crate::bed::strand;
-// use bio_types::strand;
 
 use bio_types::strand::{Strand};
 
-/* 
-STRUCTs
-
-We structure what our variable should be (strings or path)
-the "#[derive(StructOpt)]" is a macro to generate code for "clap" - which parses command line arguments
-we then only need to call the name of struct (Cli) to the ::from_args() function and done (see main)
-*/
 
 
 #[derive(StructOpt)]
 #[structopt(name = "seqrs - sequence repair in rust", about = "Quickly extract primerpairs to amplify missing/masked regions of genomes.")]
 struct Argparser {
-    /// String input for the primer version
-    #[structopt(short, long, default_value = "custom")]
-    articversion: String,
-    /// File input of the primer bed file
+    /// bed file containing the primer infos
     #[structopt(parse(from_os_str), short, long, required_if("articversion", "custom"))]
     primerbed: std::path::PathBuf,
-    /// File input for genomes
+
+    /// Fasta file input
     #[structopt(parse(from_os_str), short, long)]
     genomes: std::path::PathBuf,
+
+    /// tab separated output
+    #[structopt(short, long, default_value = "results.tsv")]
+    results: String,
+
+    /// amplicon size
+    #[structopt(short, long, default_value = "1200")]
+    ampliconsize: usize,
 
 /*
     /// Output file, stdout if not present
@@ -76,17 +73,9 @@ fn main() -> Result<()> {
         // we call the arguments from our Cli struct program above 
         let args = Argparser::from_args();
 
-        // opening the file and read it with clean error codes
-        let filecontent = std::fs::read_to_string(&args.genomes)
+        // try opening the file and read it with clean error codes
+        let _filecontent = std::fs::read_to_string(&args.genomes)
                 .with_context(|| format!("could not read file `{}`", &args.genomes.to_str().unwrap()))?;
-
-
-        // itterate through the file
-        for line in filecontent.lines() {
-                if line.contains(&args.articversion) {
-                        println!("{}", line);
-                }
-        }
 
     
         /*************
@@ -99,78 +88,98 @@ fn main() -> Result<()> {
         ************/
 
         // open the fasta and bed file
-        let mut fastarecords = fasta::Reader::new(File::open(&args.genomes)?).records();
+        //let mut reader = fasta::Reader::new(File::open(&args.genomes)?);
         let mut bedfile = bed::Reader::new(File::open(&args.primerbed)?);
 
-        // for statistics
-        let mut nb_reads = 0;
-        let mut nb_bases = 0;
+        // stringbuffer
         let mut stringbuffer = String::new();
-
-        // quick auto remove
-        fs::remove_file("./my-file.txt")?;
 
         // storing data       
         let mut fileoutput = OpenOptions::new()
                                 .write(true)
                                 .create_new(true)
                                 .append(true)
-                                .open("./my-file.txt")
-                                .unwrap();
+                                .open(&args.results)
+                                .with_context(|| format!("Could write to the output file `{}`", &args.results))?;
 
+        // Terminal prints for the user regarding their input
+        let mut nb_reads = 0;
+        let mut nb_bases = 0;
+
+        for result in fasta::Reader::new(File::open(&args.genomes)?).records() {
+                let record = result.expect("Error during fasta record parsing");
+                println!("Found Fasta: {}", record.id());
+            
+                nb_reads += 1;
+                nb_bases += record.seq().len();
+        }
+        println!("Total number of genomes found: {}", nb_reads);
+        println!("Total number of basepairs to process: {}", nb_bases);
 
         // iterate over fasta records
-        while let Some(Ok(record)) = fastarecords.next() {
-            nb_reads += 1;
-            nb_bases += record.seq().len();
-            println!("{}", record.id());
 
-                // terminal help - tells you all the "Ns" found
-                
-                println!("\x1b[0;31m____These are the N positions____\x1b[0m");
-                // for (count, _v) in record.seq().iter().enumerate().filter(|&(_, c)| *c == 78) {
-                //         println!("{}: {}", record.id(), count);
-                // }
-                
-
-                // get Primers function
-                println!("\x1b[0;31m____Iterating through the primer sets____\x1b[0m");
                 for recordbed in bedfile.records() {
                         let recorddata = recordbed.expect("Error reading record.");
+
                         match recorddata.strand() {
+                        // Forward Strand Primer
                         Some(Strand::Forward) => { 
-                                // We search for every "N" (index 78) within each record.seq()
+                                // we feed the full fasta file records into the loop 
+                                for result in fasta::Reader::new(File::open(&args.genomes)?).records() {
+                                        let record = result.expect("Error during fasta record parsing");
 
-                                for (count, _v) in record.seq().iter().enumerate().filter(|&(_, c)| *c == 78) {
-                                        let primerend = recorddata.end() as usize;
-                                        // we search now for appropriate forward primers
-                                        if primerend < count && count - primerend < 1200 && count > 100 {
-                                                //println!("## Fasta:{} has 'N' at position {} is greater than {} of FPrimer {:?}", record.id(), count, recorddata.end(), recorddata.name());
+                                        // development help
+                                        /* println!("Proccessing fasta: {} with length {}", record.id(), record.seq().len());*/
 
-                                                // store strings that we need
-                                                stringbuffer.push_str(record.id());
-                                                stringbuffer.push_str("\t");
-                                                stringbuffer.push_str(recorddata.name().unwrap());
-                                                stringbuffer.push_str("\n");
+                                        // we get every "N" position now and then we check for primers in the range of 1200bp
+                                        for (count, _v) in record.seq().iter().enumerate().filter(|&(_, c)| *c == 78) {
+                                                // converting the data to usize to allow comparision
+                                                let primerend = recorddata.end() as usize;
+                                                // we search now for appropriate forward primers
+                                                if primerend < count && count - primerend < args.ampliconsize && count > 100 {
 
-                                                // if let Err(e) = writeln!(fileoutput, "{}", recorddata.name().unwrap()) {
-                                                //         eprintln!("Couldn't write to file: {}", e);
-                                                //     }
+                                                        // store the results into a strings buffer
+                                                        stringbuffer.push_str(record.id());
+                                                        stringbuffer.push_str("\t");
+                                                        stringbuffer.push_str(recorddata.name().unwrap());
+                                                        stringbuffer.push_str("\n");
+
+                                                        // development help
+                                                        /* println!("## Fasta:{} has 'N' at position {} is greater than {} of FPrimer {:?}", record.id(), count, recorddata.end(), recorddata.name()); */
+                                                }
                                         }
                                 }
                         },
-
+                        // Reverse Strand Primer
                         Some(Strand::Reverse) => { 
-                                for (count, _v) in record.seq().iter().enumerate().filter(|&(_, c)| *c == 78) {
-                                        let primerstart = recorddata.start() as usize;
-                                        // we search now for appropriate forward primers
-                                        // missing for the if statment : count < (fastalength - 100)
-                                        if primerstart > count && primerstart - count < 1200 {
-                                                //println!("## Fasta:{} has 'N' at position {} is smaller than {} of RPrimer {:?}", record.id(), count, recorddata.start(), recorddata.name());
-                                                
+                                // we feed the full fasta file records into the loop 
+                                for result in fasta::Reader::new(File::open(&args.genomes)?).records() {
+                                        let record = result.expect("Error during fasta record parsing");
+
+                                        // development help
+                                        /* println!("Proccessing fasta: {} with length {}", record.id(), record.seq().len());*/
+
+                                        // we get every "N" position now and then we check for primers in the range of 1200bp
+                                        for (count, _v) in record.seq().iter().enumerate().filter(|&(_, c)| *c == 78) {
+                                                // converting the data to usize to allow comparision
+                                                let primerstart = recorddata.start() as usize;
+                                                // we search now for appropriate forward primers
+                                                // missing for the if statment : count < (fastalength - 100)
+                                                if primerstart > count && primerstart - count < args.ampliconsize {
+
+                                                        // store the results into a strings buffer
+                                                        stringbuffer.push_str(record.id());
+                                                        stringbuffer.push_str("\t");
+                                                        stringbuffer.push_str(recorddata.name().unwrap());
+                                                        stringbuffer.push_str("\n");
+
+                                                        // development help
+                                                        /* println!("## Fasta:{} has 'N' at position {} is greater than {} of FPrimer {:?}", record.id(), count, recorddata.start(), recorddata.name()); */
+                                                }
                                         }
                                 }
                         },
+
 
                         Some(Strand::Unknown) => println!("{:?}", recorddata.name()),
                         _ => continue,
@@ -179,54 +188,26 @@ fn main() -> Result<()> {
 
 
 
-
-
-                // CLEAN bed file loop
-                // for record in bedfile.records() {
-                //         // unwraping the record, all fields are here: https://docs.rs/bio/0.33.0/bio/io/bed/struct.Record.html
-                //         // using .expect instead of .unwrap to include error code
-                //         let recorddata = record.expect("Error reading record.");
-                        
-
-
-                //         match recorddata.strand() {
-                //         Some(Strand::Forward) => println!("{:?} is {:?} with end {}", recorddata.name(), recorddata.strand(), recorddata.end()),
-                //         Some(Strand::Reverse) => println!("{:?} is {:?} with start {}", recorddata.name(), recorddata.strand(), recorddata.start()),
-                //         Some(Strand::Unknown) => println!("{:?}", recorddata.name()),
-                //         _ => println!("no value"),
-                //         }
-                // //writer.write(&rec).expect("Error writing record.");
-                // };     
-            
-   
-                // I NEED HERE NOW TO FIND THE MINIMUM OF ALL FORWARDS STRANDS
-                // makes sense to work here with a data frame like https://docs.rs/polars/0.12.1/polars/
-                // or https://github.com/nevi-me/rust-dataframe
-                // but check if i can somehow work / math with them in a good way
-   
-   
-           
-        }
+     
         
-        // Terminal prints for the user regarding their input
-        println!("Number of genomes: {}", nb_reads);
-        println!("Total number of bases: {}", nb_bases);
 
-        //let src3: String = String::from(r#"o{"livia"}"#);
+
+        // Storing all strings into a vector to get rid of dublicates
         let l = stringbuffer;
         let l = l.split("\n");
         let mut vec: Vec<&str> = l.collect();
         vec.sort() ;
         vec.dedup() ;
-        vec.remove(0) ;
-        println!("{:?}", vec);
+        //vec.remove(0) ;
+        //println!("{:?}", vec);
 
+        // write vector to file
         for i in vec.iter(){                                                                                                                                                   
                 if let Err(e) = writeln!(fileoutput, "{}", i) {
                 eprintln!("Couldn't write to file: {}", e);
                 }                                                                                                                           
             }  
-
+        
         Ok(())
 }
 
